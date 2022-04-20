a47e9fbcebdc9440cfef6530df1f2acfc7e12301
isarablanco <20170203@ce.pucmm.edu.do>
2022-04-20 09:31:59 -0400

    upload mio

--- assets/demo/chart-bar-demo.js
+++ assets/demo/chart-bar-demo.js
@@ -7,12 +7,12 @@ var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"],
     datasets: [{
       label: "Revenue",
-      backgroundColor: "rgba(2,117,216,1)",
-      borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+      borderColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"],
+      data: [4, 8, 10, 20, 21, 35],
     }],
   },
   options: {
@@ -31,7 +31,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 100,
           maxTicksLimit: 5
         },
         gridLines: {
--- assets/demo/chart-pie-demo.js
+++ assets/demo/chart-pie-demo.js
@@ -3,14 +3,29 @@ Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSyste
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
 // Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
+// var ctx = document.getElementById("myPieChart");
+// var myPieChart = new Chart(ctx, {
+//   type: 'pie',
+//   data: {
+//     labels: ["Blue", "Red", "Yellow", "Green"],
+//     datasets: [{
+//       data: [12.21, 15.58, 11.25, 8.32],
+//       backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
+//     }],
+//   },
+// });
+var ctxP = document.getElementById("myPieChart").getContext('2d');
+var myPieChart = new Chart(ctxP, {
   type: 'pie',
   data: {
-    labels: ["Blue", "Red", "Yellow", "Green"],
+    labels: ["Sonrisa", "Sorpresa", "Ceño Fruncido", "Inflar Cachetes", "Lanzar Beso"],
     datasets: [{
-      data: [12.21, 15.58, 11.25, 8.32],
-      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
-    }],
+      data: [30, 10, 5, 3, 6],
+     backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+     hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+    }]
   },
+  options: {
+    responsive: true
+  }
 });
